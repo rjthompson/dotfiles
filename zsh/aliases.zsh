a685f5b37bed4ec93114e8f5b410dbe5534a479e
rjthompson <r.j.thompson3@ncl.ac.uk>
2016-11-02 16:44:05 +0000

    update to make use of colors in ls

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,3 +2,8 @@ alias reload!='. ~/.zshrc'
 
 alias clc='clear' # Good 'ol Clear Screen command
 alias tree='tree -C' # Add colors to tree output
+
+if [[ `uname` == 'Darwin' ]]; then
+else
+    alias ls='ls --color'
+fi
