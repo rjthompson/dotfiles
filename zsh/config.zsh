8a1b714191910323aa43c427d69a6709faa23d19
Rob Thompson <r.j.thompson3@ncl.ac.uk>
2016-11-22 10:54:02 -0500

    configure keybindings to work with vi mode in commandline

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -53,13 +53,6 @@ bindkey -v
 bindkey '^P' up-history
 bindkey '^N' down-history
 bindkey '^r' history-incremental-search-backward
+bindkey '^A' beginning-of-line
 
 export KEYTIMEOUT=1
-
-bindkey '^[^[[D' backward-word
-bindkey '^[^[[C' forward-word
-bindkey '^[[5D' beginning-of-line
-bindkey '^[[5C' end-of-line
-bindkey '^[[3~' delete-char
-bindkey '^[^N' newtab
-bindkey '^?' backward-delete-char
